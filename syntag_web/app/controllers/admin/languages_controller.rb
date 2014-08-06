b8588a088e440266dcfc24fc4a87c4a7a235fd6b
Jack McCracken <jackamc19@gmail.com>
2014-08-05 20:08:56 -0400

    Renamed syntag to syntag_web to avoid ambiguity in the future

--- syntag_web/app/controllers/admin/languages_controller.rb
+++ syntag_web/app/controllers/admin/languages_controller.rb
@@ -1,10 +1,28 @@
 class Admin::LanguagesController < ApplicationController
   def index
     @languages = Language.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @languages }
+    end
   end
 
   def new
     @language = Language.new
+
+    respond_to do |format|
+      format.html { render 'new' }
+      format.json { render json: @language }
+    end
+  end
+
+  def show
+    @language = Language.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @language }
+    end
   end
 
   def create
--- syntag_web/app/controllers/snippets_controller.rb
+++ syntag_web/app/controllers/snippets_controller.rb
@@ -1,15 +1,29 @@
 class SnippetsController < ApplicationController
   def index
     @snippets = Snippet.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @snippets }
+    end
   end
   
   def new
     @snippet = Snippet.new
   end
 
+  def show
+    @snippet = Snippet.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @snippet }
+    end
+  end
+  
   def create
     @snippet = Snippet.new
-    
+
     @snippet.title = params[:snippet][:title]
     @snippet.contents = params[:snippet][:contents]
     @snippet.language_id = params[:snippet][:language_id]
