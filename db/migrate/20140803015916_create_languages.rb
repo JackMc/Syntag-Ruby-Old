60848a0d5a5cf30319f0a9cd4030dab8dc8675f5
Jack McCracken <jackamc19@gmail.com>
2014-08-10 04:30:05 -0400

    Removed unused highlighter_name from language table

--- db/migrate/20140803015916_create_languages.rb
+++ db/migrate/20140803015916_create_languages.rb
@@ -2,7 +2,6 @@ class CreateLanguages < ActiveRecord::Migration
   def change
     create_table :languages do |t|
       t.string :name
-      t.string :highlighter_name
       t.string :code_editor_name
 
       t.timestamps
