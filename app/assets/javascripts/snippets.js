274cc1bbd1edffe71ed17e989ce2746eb4594834
Jack McCracken <jackamc19@gmail.com>
2014-08-10 05:51:04 -0400

    Bug fixes, links.

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -3,16 +3,28 @@ var downloaded = []
 $(document).on("page:change", function () {
     var editor = null
 
-    function set_language(json_lang) {
-	// TODO: Here we assume that it succeeded. Can we do better?
-	if (downloaded.indexOf(json_lang["id"]) < 0) {
-	    $("head").append($("<script src=\"/assets/codemirror/modes/" + json_lang["code_editor_name"] + ".js\"></script>"))
+    function set_language(lang_id) {
+	var code_editor_name = null
+
+	// If the language has already been downloaded (along with it's JSON)
+	if (lang_id < downloaded.length && downloaded[lang_id] != undefined) {
+	    code_editor_name = downloaded[lang_id]
+	    
+	    if (editor != null) {
+		editor.setOption("mode", code_editor_name)
+	    }
 	}
-	
-	downloaded.push(json_lang["id"])
-	
-	if (editor != null) {
-	    editor.setOption("mode", json_lang["code_editor_name"])
+	else {
+	    $.ajax("/admin/languages/" + lang_id + ".json").done(function (json_lang) {
+		$("head").append($("<script src=\"/assets/codemirror/modes/" + json_lang["code_editor_name"] + ".js\"></script>"))
+		downloaded[json_lang["id"]] = json_lang["code_editor_name"]
+
+		code_editor_name = json_lang["code_editor_name"]
+
+		if (editor != null) {
+		    editor.setOption("mode", code_editor_name)
+		}
+	    })
 	}
     }
     
@@ -22,20 +34,17 @@ $(document).on("page:change", function () {
 	function download_selected_snippet() {
 	    $.ajax("/snippets/" + $("#snippet_select").val() + ".json").done(function (json_snip) {
 		editor.getDoc().setValue(json_snip["contents"])
-		
-		$.ajax("/admin/languages/" + json_snip["language_id"] + ".json").done(function (json_lang) {
-		    $("#lang").text(json_lang["name"])
-
-		    set_language(json_lang)
-		})
+		set_language($("#snippet_select").val())
 	    })
 	}
 
 	$("#snippet_select").change(download_selected_snippet)
 
-	editor = CodeMirror.fromTextArea($("#snippet")[0], {
-	    lineNumbers: false,
-	})
+	if ($(".CodeMirror").length === 0) {
+	    editor = CodeMirror.fromTextArea($("#snippet")[0], {
+		lineNumbers: false,
+	    })
+	}
 	
 	download_selected_snippet()
     }
@@ -44,17 +53,17 @@ $(document).on("page:change", function () {
 	function load_selected_language() {
 	    snippet_lang_id = $("#snippet_language_id").val()
 	    // <script data-turbolinks-track="true" src="/assets/codemirror/modes/ruby.js?body=1"></script>
-            $.ajax("/admin/languages/" + snippet_lang_id + ".json").done(function (json_lang) {
-		set_language(json_lang)
-	    })
+            set_language(snippet_lang_id)
 	}
 
 	$("#snippet_language_id").change(load_selected_language)
 
 	load_selected_language()
 
-	editor = CodeMirror.fromTextArea($("#snippet_contents")[0], {
-	    lineNumbers: true,
-	})
+	if ($(".CodeMirror").length === 0) {
+	    editor = CodeMirror.fromTextArea($("#snippet_contents")[0], {
+		lineNumbers: true,
+	    })
+	}
     }
 })
